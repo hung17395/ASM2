4f9b2df5a49e8e0a752af69110cc711030a82737
ShaunDat <datvmbhaf190170@fpt.edu.vn>
2022-08-12 10:30:22 +0700

    ViDat Update ver2.1

--- resources/views/Admin/userList.blade.php
+++ resources/views/Admin/userList.blade.php
@@ -7,6 +7,7 @@
       <div class="card">
         <div class="card-header">
           <h4 class="card-title">User Table</h4>
+          {{-- <a href="{{asset('/user/create')}}" class="btn btn-primary edit"><span class="glyphicon glyphicon-edit"> </span> Add</a> --}}
         </div>
         <div class="card-body">
           <div class="table-responsive">
@@ -35,7 +36,7 @@
                       <td>{{ $value->email}}</td>
                       <td class="text-right">
                           <a href="{{asset('editUser/'.$value->id)}}" class="btn btn-primary edit"><span class="glyphicon glyphicon-edit"> </span> Edit</a>
-                          <a href="{{asset('delete/'.$value->id)}}" onclick="return confirm('Bạn có chắc muốn xóa?')" class="btn btn-danger"><span class="glyphicon glyphicon-trash"> </span>Delete</a>
+                          <a href="{{asset('user/delete/'.$value->id)}}" onclick="return confirm('Bạn có chắc muốn xóa?')" class="btn btn-danger"><span class="glyphicon glyphicon-trash"> </span>Delete</a>
                         </td>
                     </tr>
                     @endforeach
